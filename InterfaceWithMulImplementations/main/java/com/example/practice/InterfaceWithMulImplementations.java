ffb7df67d40a488cfebe9ba6bfaf29df7e6c2307
muthu-Sowmiya-m <83859464+muthu-Sowmiya-m@users.noreply.github.com>
2022-04-05 17:04:25 +0530

    4/4/22 #1

--- InterfaceWithMulImplementations/main/java/com/example/practice/InterfaceWithMulImplementations.java
+++ InterfaceWithMulImplementations/main/java/com/example/practice/InterfaceWithMulImplementations.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 
+
 @SpringBootApplication
 public class InterfaceWithMulImplementations {
 
